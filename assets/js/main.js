1f8135b6a87c043b3c407bcd29136d174d265b95
Emma Phommatha <emma.dvlpr@outlook.com>
2020-09-21 14:38:40 -0400

    add top section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,45 @@
 jQuery(document).ready(function($){
+	if ( $( '.with-carousel' ).length > 0 ) {
+		var owl = $('.with-carousel .owl-carousel');
+		owl.owlCarousel({
+		    loop:true,
+		    nav:true,
+		    autoplay: true,
+		    smartSpeed: 1200,
+		    
+		    navSpeed: 1200,
+		    dotsSpeed: 1200,
+		    margin:90,
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        767:{
+		            items:1,
+		            margin:15
+		        },            
+		        992:{
+		            items:2,
+		            margin:30
+		        }
+		    }
+		});
+		
+	}
 	
-	$(".navbar-toggle").click(function () { 
-		$(this).toggleClass('open');
+
+	
+	if ( $( '.slider' ).length > 0 ) {
+	$('.slider').owlCarousel({
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    items:1,
+	    loop:true,
+	    autoplay: true,
+	    smartSpeed: 900,
+	    margin:30,
+	   
 	});
+	}
 
 });
